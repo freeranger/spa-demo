73e5e52833628df62f28e4a0d50705a3cb9fc1cd
freeranger <tony.butler@me.com>
2017-01-21 18:33:41 +0000

    Restrict Cities to GET only

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   scope :api, defaults: { format: :json} do
     resources :foos, except: [:new, :edit]
     resources :bars, except: [:new, :edit]
-    resources :cities, except: [:new, :edit]
+    resources :cities, only: [:index, :show]
   end
 
   get '/ui' => 'ui#index'
