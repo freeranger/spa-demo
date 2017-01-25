60561020718d5e2cd074be7fb729d077210a36e3
freeranger <tony.butler@me.com>
2017-01-25 12:16:33 +0000

    Redirection added when you go to root of api site

--- app/controllers/ui_controller.rb
+++ app/controllers/ui_controller.rb
@@ -1,2 +1,7 @@
 class UiController < ApplicationController
+  def index
+    redirect_to "/client/index.html", :status=>:moved_permanently
+    # if deploying to somewhere separate
+    # redirect_to "https://freeranger.github.io/spa-demo/public/client/index.html"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,9 @@ Rails.application.routes.draw do
     resources :states, only: [:index, :show]
   end
 
-  get "/client-assets/:name.:format", :to => redirect "/client/client-assets/%{name}.%{format}"
+  get "/client-assets/:name.:format", :to => redirect("/client/client-assets/%{name}.%{format}")
+  get "/", :to => redirect("/client/index.html")
+
   get '/ui' => 'ui#index'
   get '/ui#' => 'ui#index'
   root 'ui#index'
